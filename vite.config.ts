5e96d282fe48ff264bffc43c3a3d92e9e245652d
pratikmastud27-ops <pratikmastud27@gmail.com>
2026-07-27 14:32:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/creator-ai/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
